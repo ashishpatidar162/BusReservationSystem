4fa2e54ff2156af97bc98ddb5cb44dd86c5b4fdf
ashishpatidar162 <patidarashish162>
2020-01-31 20:38:51 +0530

    initial commit

--- Springboot-starterkit/src/main/java/com/busreservationsystem/application/serviceImp/ServiceImpl.java
+++ Springboot-starterkit/src/main/java/com/busreservationsystem/application/serviceImp/ServiceImpl.java
@@ -1,5 +1,9 @@
 package com.busreservationsystem.application.serviceImp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,8 @@ import com.busreservationsystem.application.dto.common.AgencyDto;
 import com.busreservationsystem.application.model.AgencyModel;
 import com.busreservationsystem.application.repository.AgencyRepository;
 import com.busreservationsystem.application.service.Service;
+
+
 
 @Component
 public class ServiceImpl implements Service {
@@ -28,5 +34,22 @@ public class ServiceImpl implements Service {
 				
 				return savedData;
 		
+	}
+	public List<AgencyModel> getAgency(){
+		
+	List<AgencyDto>	agencyListDto=agencyRepository.findAll();
+	
+	List<AgencyModel> savedData=new ArrayList<>();
+	    //convert DTO to Model
+	ListIterator list=agencyListDto.listIterator();
+	while(list.hasNext()) {
+		
+		    AgencyDto   ADTO=(AgencyDto)list.next();
+		    
+	   savedData= mapper.map(ADTO, AgencyModel.class);
+	  
+	   
+	     return savedData;
+	}
 	}
 }
